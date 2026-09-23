7a5879208ee41abd3856c27ce44f1b4891e9bc5d
Jovan Markovic <miagi@vivaldi.net>
2026-09-23 09:36:36 +0200

    Improved ZLS codebase lookup

--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const _zig_main = @import("main.zig"); // for ZLS referece searches
 
 pub const parser = @import("parser.zig");
 pub const forward = @import("forward.zig");
